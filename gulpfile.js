824fce5bb1a9b1145e2d7e83c1498b5631ff38f2
Matthew Crossland <mpcrossland@gmail.com>
2017-06-10 14:35:39 -0400

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,11 @@ const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
 
 gulp.task('styles', () => {
-    return gulp.src('./dev/styles/**/*.scss')
+    return gulp.src('dev/styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('style.css'))
-        .pipe(gulp.dest('./public/styles'))
+        .pipe(gulp.dest('public/styles'))
+        .pipe(reload({stream:true}));
 });
 
 gulp.task('js', () => {
